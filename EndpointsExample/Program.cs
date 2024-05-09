1ec77c88a8dfc96468b95ae9a0570a2275d49bd5
ShatrudhanKr5 <shatrudhankr5@gmail.com>
2024-05-09 23:45:57 +0530

    Routing

--- EndpointsExample/Program.cs
+++ EndpointsExample/Program.cs
@@ -1,14 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-
+app.UseStaticFiles();
 app.UseRouting();
 
-app.Use(async (HttpContext httpContext,RequestDelegate requestDelegate) =>
+app.Use(async (HttpContext httpContext, RequestDelegate next) =>
 {
-     Endpoint? endpoint= httpContext.GetEndpoint();
-    await httpContext.Response.WriteAsync(endpoint?.DisplayName);
-    await requestDelegate(httpContext);
+    Endpoint? endpoint = httpContext.GetEndpoint();
+    string requesturl = endpoint?.DisplayName ?? "np";
+    // await httpContext.Response.WriteAsync($"{requesturl}{Environment.NewLine}");
+    await next(httpContext);
 });
 app.UseEndpoints(endpoints =>
 {
@@ -20,6 +21,14 @@ app.UseEndpoints(endpoints =>
     {
         await context.Response.WriteAsync(context.Request.Path);
     });
+
+    endpoints.Map("files/{filename}.{extension}", async (HttpContext context) =>
+    {
+        string fileName =context.Request.RouteValues["filename"].ToString();
+        string extension = context.Request.RouteValues["extension"].ToString();
+        await context.Response.WriteAsync($"Filename= {fileName}, Extension= {extension}");
+    });
+
 });
 
 app.MapGet("/",()=>"Hello");
